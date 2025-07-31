05d8d38186ccbfd8f685d68ed646d2a454aed15f
anujeetverma <167993759+anujeetverma@users.noreply.github.com>
2025-07-31 09:25:11 +0530

    day2

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,7 @@
 print("My age: " + str(12))
+print(5/3)
+print(5//3)
+print(5%2)
+print(5**2)
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)
--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,7 @@
+
 bmi = 84 / 1.65 ** 2
+print(round(bmi,2))
+score=0
+is_winning=False
+# fstring
+print(f"your score is {score} and your team is {is_winning}")
