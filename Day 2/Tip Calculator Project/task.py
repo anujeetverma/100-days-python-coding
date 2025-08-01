f07e388652f7fd9dab3ddb696b7f3bf8d560a5ba
anujeetverma <167993759+anujeetverma@users.noreply.github.com>
2025-08-01 18:27:36 +0530

    day2

--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,4 @@ bill = float(input("What was the total bill? $ "))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 due_val = (bill + bill*tip/100)/people
-print(f"Each person should pay : {round(due_val,2)}")
+print(f"Each person should pay : {due_val:.2f}")
