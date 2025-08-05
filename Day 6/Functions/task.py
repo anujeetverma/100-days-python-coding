f1fce8a20da211cd4497bacc12aa46da9c9ac235
anujeetverma <167993759+anujeetverma@users.noreply.github.com>
2025-08-05 22:55:51 +0530

    day6

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -7,3 +7,27 @@ def my_function():
     print("Bye")
 
 my_function()
+
+# #wrote lots of Reeborg's world Code
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     while wall_on_right() and front_is_clear() and not at_goal():
+#         move()
+#     if not at_goal():
+#         if wall_in_front() and right_is_clear():
+#             turn_right()
+#             move()
+#         elif right_is_clear() and front_is_clear():
+#             turn_right()
+#             move()
+#         elif wall_in_front() and wall_on_right():
+#             turn_left()
+#         elif front_is_clear():
+#             move()
+#         elif wall_in_front():
+#             turn_right()
