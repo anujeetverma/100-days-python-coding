2b9f36601ae309ba16861bb109f5910590e7326b
anujeetverma <167993759+anujeetverma@users.noreply.github.com>
2025-08-06 20:53:12 +0530

    day7

--- Day 6/Functions/task.py
+++ Day 6/Functions/task.py
@@ -14,6 +14,16 @@ my_function()
 #     turn_left()
 #     turn_left()
 #
+# #
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while front_is_clear():
+#     move()
+# turn_left()
 #
 # while not at_goal():
 #     while wall_on_right() and front_is_clear() and not at_goal():
