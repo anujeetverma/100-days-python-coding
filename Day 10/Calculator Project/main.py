6d2c427fd9051d6dab5d25226c720344e16e6997
anujeetverma <167993759+anujeetverma@users.noreply.github.com>
2025-08-11 23:20:49 +0530

    day10

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,30 @@
+import art
+print(art.logo)
 def add(n1, n2):
     return n1 + n2
+def sub(n1, n2):
+    return n1 - n2
+def mul(n1, n2):
+    return n1 * n2
+def div(n1, n2):
+    return n1 / n2
+n1 = int(input("What's the first number: "))
+cont = "y"
+while cont == "y":
 
-
+    print("+")
+    print("-")
+    print("*")
+    print("/")
+    operation = input("Pick an operation: ")
+    n2 = int(input("What's the next number: "))
+    if operation == "+":
+        n1 = add(n1 , n2)
+    elif operation == "-":
+        n1 = sub(n1 , n2)
+    elif operation == "*":
+        n1 = mul(n1 , n2)
+    elif operation == "/":
+        n1 = div(n1 , n2)
+    cont = input(f"Type 'y' to continue with {n1} or type 'n' to stop the calculation")
+print(f"The final answer = {n1}")
--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,5 +1,5 @@
 def title(f_name, l_name):
-    name= f_name.upper() + l_name.upper()
+    name= f_name.title() + l_name.title()
     return name
 
-print(title("anujeet"," verma"))
+print(title("anujeet"," VERMa"))
